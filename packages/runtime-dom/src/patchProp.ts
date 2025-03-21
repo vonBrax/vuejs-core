efe0244b9b632f324c85b25cd86d8f491653efb2
Alexandre von Brasche Figueiredo <alexandre.vonbraschefigueiredo@aconso.com>
2025-03-21 12:01:41 +0100

    perf(runtime-dom): avoid using in operator twice

--- packages/runtime-dom/src/patchProp.ts
+++ packages/runtime-dom/src/patchProp.ts
@@ -10,7 +10,10 @@ import {
   isOn,
   isString,
 } from '@vue/shared'
-import type { RendererOptions } from '@vue/runtime-core'
+import type {
+  ComponentInternalInstance,
+  RendererOptions,
+} from '@vue/runtime-core'
 import type { VueElement } from './apiCustomElement'
 
 const isNativeOn = (key: string) =>
@@ -31,51 +34,92 @@ export const patchProp: DOMRendererOptions['patchProp'] = (
   parentComponent,
 ) => {
   const isSVG = namespace === 'svg'
+
   if (key === 'class') {
     patchClass(el, nextValue, isSVG)
-  } else if (key === 'style') {
+    return
+  }
+
+  if (key === 'style') {
     patchStyle(el, prevValue, nextValue)
-  } else if (isOn(key)) {
+    return
+  }
+
+  if (isOn(key)) {
     // ignore v-model listeners
     if (!isModelListener(key)) {
       patchEvent(el, key, prevValue, nextValue, parentComponent)
     }
-  } else if (
-    key[0] === '.'
-      ? ((key = key.slice(1)), true)
-      : key[0] === '^'
-        ? ((key = key.slice(1)), false)
-        : shouldSetAsProp(el, key, nextValue, isSVG)
-  ) {
+    return
+  }
+
+  const isPropShorthand = key.startsWith('.')
+  const isAttrShorthand = key.startsWith('^')
+
+  if (isPropShorthand || isAttrShorthand) {
+    key = key.slice(1)
+  }
+
+  if (isPropShorthand) {
     const camelKey = camelize(key)
     const propKey = camelKey in el ? camelKey : key
-    patchDOMProp(el, propKey, nextValue, parentComponent, key)
-    // #6007 also set form state as attributes so they work with
-    // <input type="reset"> or libs / extensions that expect attributes
-    // #11163 custom elements may use value as an prop and set it as object
-    if (
-      !el.tagName.includes('-') &&
-      (key === 'value' || key === 'checked' || key === 'selected')
-    ) {
-      patchAttr(el, key, nextValue, isSVG, parentComponent, key !== 'value')
+    handleProp(el, propKey, key, nextValue, parentComponent, isSVG)
+    return
+  }
+
+  if (!isAttrShorthand) {
+    const result = shouldSetAsProp(el, key, nextValue, isSVG)
+    if (result.isProp) {
+      handleProp(el, result.propName, key, nextValue, parentComponent, isSVG)
+      return
     }
-  } else if (
+  }
+
+  if (
     // #11081 force set props for possible async custom element
     (el as VueElement)._isVueCE &&
     (/[A-Z]/.test(key) || !isString(nextValue))
   ) {
     patchDOMProp(el, camelize(key), nextValue, parentComponent, key)
-  } else {
-    // special case for <input v-model type="checkbox"> with
-    // :true-value & :false-value
-    // store value as dom properties since non-string values will be
-    // stringified.
-    if (key === 'true-value') {
-      ;(el as any)._trueValue = nextValue
-    } else if (key === 'false-value') {
-      ;(el as any)._falseValue = nextValue
-    }
-    patchAttr(el, key, nextValue, isSVG, parentComponent)
+    return
+  }
+
+  // special case for <input v-model type="checkbox"> with
+  // :true-value & :false-value
+  // store value as dom properties since non-string values will be
+  // stringified.
+  if (key === 'true-value') {
+    ;(el as any)._trueValue = nextValue
+  } else if (key === 'false-value') {
+    ;(el as any)._falseValue = nextValue
+  }
+  patchAttr(el, key, nextValue, isSVG, parentComponent)
+}
+
+function handleProp(
+  el: Element,
+  propKey: string,
+  attrKey: string,
+  nextValue: any,
+  parentComponent: ComponentInternalInstance | null | undefined,
+  isSVG: boolean,
+) {
+  patchDOMProp(el, propKey, nextValue, parentComponent, attrKey)
+  // #6007 also set form state as attributes so they work with
+  // <input type="reset"> or libs / extensions that expect attributes
+  // #11163 custom elements may use value as an prop and set it as object
+  if (
+    !el.tagName.includes('-') &&
+    (attrKey === 'value' || attrKey === 'checked' || attrKey === 'selected')
+  ) {
+    patchAttr(
+      el,
+      attrKey,
+      nextValue,
+      isSVG,
+      parentComponent,
+      attrKey !== 'value',
+    )
   }
 }
 
@@ -84,18 +128,22 @@ function shouldSetAsProp(
   key: string,
   value: unknown,
   isSVG: boolean,
-) {
+): { isProp: boolean; propName: string } {
+  const result = { isProp: false, propName: key }
+
   if (isSVG) {
     // most keys must be set as attribute on svg elements to work
     // ...except innerHTML & textContent
     if (key === 'innerHTML' || key === 'textContent') {
-      return true
+      result.isProp = true
+      return result
     }
     // or native onclick with function values
     if (key in el && isNativeOn(key) && isFunction(value)) {
-      return true
+      result.isProp = true
+      return result
     }
-    return false
+    return result
   }
 
   // these are enumerated attrs, however their corresponding DOM properties
@@ -105,23 +153,23 @@ function shouldSetAsProp(
   // Note that `contentEditable` doesn't have this problem: its DOM
   // property is also enumerated string values.
   if (key === 'spellcheck' || key === 'draggable' || key === 'translate') {
-    return false
+    return result
   }
 
   // #1787, #2840 form property on form elements is readonly and must be set as
   // attribute.
   if (key === 'form') {
-    return false
+    return result
   }
 
   // #1526 <input list> must be set as attribute
   if (key === 'list' && el.tagName === 'INPUT') {
-    return false
+    return result
   }
 
   // #2766 <textarea type> must be set as attribute
   if (key === 'type' && el.tagName === 'TEXTAREA') {
-    return false
+    return result
   }
 
   // #8780 the width or height of embedded tags must be set as attribute
@@ -133,14 +181,22 @@ function shouldSetAsProp(
       tag === 'CANVAS' ||
       tag === 'SOURCE'
     ) {
-      return false
+      return result
     }
   }
 
   // native onclick with string value, must be set as attribute
   if (isNativeOn(key) && isString(value)) {
-    return false
+    return result
   }
 
-  return camelize(key) in el || key in el
+  const camelKey = camelize(key)
+  if (camelKey in el) {
+    result.isProp = true
+    result.propName = camelKey
+    return result
+  }
+
+  result.isProp = key in el
+  return result
 }
